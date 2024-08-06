a9cb21418b96ff5ee0e8af71a0909d89c4a751c1
Athar Ansari <athar8932@gmail.com>
2024-08-06 13:11:58 +0530

    update

--- src/Component/Main/Main.jsx
+++ src/Component/Main/Main.jsx
@@ -19,7 +19,7 @@ const Main = () => {
       text: " PORTFOLIO ",
     },
     {
-      url: "https://drive.google.com/uc?export=download&id=1aW_KnRmS87bCik91FPbH5ovrYdICqCyL",
+      url: "https://drive.google.com/uc?export=download&id=1G9BcsdobzToh3-1yHN6YmqK3zxHnmfmQ",
       imgSrc: Curriculum,
       text: " CV ",
     },
@@ -39,7 +39,7 @@ const Main = () => {
       text: " INSTAGRAM ",
     },
     {
-      url: "https://www.facebook.com/mdathar.ansari.547/",
+      url: "https://www.facebook.com/atharransarii/",
       imgSrc: facebook,
       text: " FACEBOOK ",
     },
